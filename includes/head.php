58f3d43bd5d43ebfbede006344c51f8ff0236084
eracool007 <viacarolinefontaine@gmail.com>
2022-04-05 10:04:27 -0400

    Meta part 2

--- includes/head.php
+++ includes/head.php
@@ -71,19 +71,9 @@ $page= "includes/main-page.php";
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/struct.css" />
-    <?php if($toprint || $printList) : ?>
-      <link rel="stylesheet" type="text/css" href="css/print.css" media="print" />
-    <?php endif; ?>
-    <script src="https://kit.fontawesome.com/146f5f72b9.js" crossorigin="anonymous"></script>
-
-    <script src="js/script.js"></script>
-    <title>V-Box : La boîte à recette végane</title>
-
-    <!-- Primary Meta Tags -->
+    
     <title>VBox - La boîte à recette végane - Un blog et des recettes saines pour optimiser votre santé</title>
+    
     <meta name="title" content="VBox - La boîte à recette végane - Un blog et des recettes saines pour optimiser votre santé">
     <meta name="description" content="Recettes véganes simples et articles de blog santé pour manger et vivre sainement en augmentant sa consommation de produits végétaux">
 
@@ -92,14 +82,28 @@ $page= "includes/main-page.php";
     <meta property="og:url" content="https://www.caroline-fontaine/vbox/">
     <meta property="og:title" content="VBox - La boîte à recette végane - Un blog et des recettes saines pour optimiser votre santé">
     <meta property="og:description" content="Recettes véganes simples et articles de blog santé pour manger et vivre sainement en augmentant sa consommation de produits végétaux">
-    <!-- <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"> -->
-
+    <meta property="og:image" content="https://www.caroline-fontaine.com/vbox/assets/image11.jpg"> 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image"> 
     <meta property="twitter:url" content="https://www.caroline-fontaine/vbox/">
     <meta property="twitter:title" content="VBox - La boîte à recette végane - Un blog et des recettes saines pour optimiser votre santé">
     <meta property="twitter:description" content="Recettes véganes simples et articles de blog santé pour manger et vivre sainement en augmentant sa consommation de produits végétaux">
-    <!--<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"> -->
+    <meta property="twitter:image" content="https://www.caroline-fontaine.com/vbox/assets/image11.jpg"> 
+
+    <!-- Mobile devices -->
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+
+    <!-- Styles ands JS -->
+    <link rel="icon" type="text/png" href="favicon-32x32.png">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/struct.css" />
+
+    <?php if($toprint || $printList) : ?>
+      <link rel="stylesheet" type="text/css" href="css/print.css" media="print" />
+    <?php endif; ?>
+    <script src="https://kit.fontawesome.com/146f5f72b9.js" crossorigin="anonymous"></script>
+
+    <script src="js/script.js"></script>
 
   </head>
   <body>
